15e2f3c2da74531dc9ae9c0b30987a8b140f894b
AngelNad <angelnad@yandex.ru>
2022-04-02 18:48:21 +0300

    Test isort4

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,8 +1,9 @@
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework import serializers  # isort:skip
+from rest_framework import serializers
 
 from users.serializers import CustomUserSerializer  # isort:skip
-from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
+from .models import (Favorite, Ingredient,  # isort:skip
+                     Recipe, RecipeIngredient,  # isort:skip
                      ShoppingCart, Tag)  # isort:skip
 
 
